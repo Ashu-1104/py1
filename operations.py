9543b8314424cac5fb8f169f45ba6491a200afe0
Ashu-1104 <ashutoshtiwari110504@gmail.com>
2025-04-29 20:42:14 +0530

    operations

---
+++ operations.py
@@ -0,0 +1,7 @@
+a=int(input("Enter First Number: "))
+b=int(input("Enter Second number: "))
+
+print("Addition = ",a+b)
+print("substraction = ",a-b)
+print("multiplication = ",a*b)
+print("division = ",a/b)
